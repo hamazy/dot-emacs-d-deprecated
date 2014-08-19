4b459504f647dc02d4bbb425c659d9bf07fde6cb
Suguru Hamazaki <hamazy@suguruhamazaki.com>
2014-08-19 18:45:12 +0900

    Use haskell-cabal in addition to haskell-mode for haskell programming

--- init.el
+++ init.el
@@ -174,13 +174,45 @@
 (my-package-install 'haskell-mode)
 (add-hook 'after-init-hook
 	  (lambda ()
-	    (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
-	    (when (fboundp 'prettify-symbols-mode)
-		(add-to-list 'prettify-symbols-alist '("<-" . 8592))
-	      (add-to-list 'prettify-symbols-alist '("->" . 8594))
-	      (add-to-list 'prettify-symbols-alist '("=>" . 8658))
-	      (add-to-list 'prettify-symbols-alist '("\\" . 955))
-	      (prettify-symbols-mode))))
+	    (add-hook 'haskell-mode-hook
+		      (lambda ()
+			(setq indent-tabs-mode nil)
+			(turn-on-haskell-indentation)
+			(turn-on-haskell-decl-scan)
+			(when (fboundp 'prettify-symbols-mode)
+			  (add-to-list 'prettify-symbols-alist '("<-" . 8592))
+			  (add-to-list 'prettify-symbols-alist '("->" . 8594))
+			  (add-to-list 'prettify-symbols-alist '("=>" . 8658))
+			  (add-to-list 'prettify-symbols-alist '("\\" . 955))
+			  (prettify-symbols-mode))))
+	    (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+	    (custom-set-variables
+	     '(haskell-process-suggest-remove-import-lines t)
+	     '(haskell-process-auto-import-loaded-modules t)
+	     '(haskell-process-log t)
+	     '(haskell-process-type 'cabal-repl))
+	    (eval-after-load "haskell-mode"
+	      '(progn
+		 (define-key haskell-mode-map (kbd "C-,") 'haskell-move-nested-left)
+		 (define-key haskell-mode-map (kbd "C-.") 'haskell-move-nested-right)
+		 (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+		 (define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+		 (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+		 (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+		 (define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+		 (define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+		 (define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
+		 (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
+		 (define-key haskell-mode-map (kbd "M-.") 'haskell-mode-jump-to-def)))
+	    (eval-after-load "haskell-cabal"
+	      '(progn
+		 (define-key haskell-cabal-mode-map (kbd "C-`") 'haskell-interactive-bring)
+		 (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+		 (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+		 (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)))
+	    (eval-after-load "which-func"
+	      '(add-to-list 'which-func-modes 'haskell-mode))))
+(my-add-to-path "~/.cabal/bin")
 
 ;; auto-complete
 (my-package-install 'auto-complete)
